c9abea6c350f7e562614f9801af64d87d1c44ec5
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2021-04-06 00:58:50 +0530

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+module.exports = (client) => {
+  console.log(
+    `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
+  );
+
+  const activities = [`All Giveaways Running Smoothly`, `ZeroSync on YouTube`];
+  setInterval(() => {
+    let activity = activities[Math.floor(Math.random() * activities.length)];
+    client.user.setActivity(activity, { type: "WATCHING" });
+  }, 20000);
+
+};
