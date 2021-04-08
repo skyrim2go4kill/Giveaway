5c77ba033e8a071bbc5ccc4f3664f8ccf69463cf
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2021-04-08 15:52:27 +0530

    Update and rename events/message.js to events/discord/message.js

--- events/discord/message.js
+++ events/discord/message.js
@@ -1,18 +1,18 @@
 module.exports = (client, message) => {
-    // Ignore all bots
+    // return if author is a bot
     if (message.author.bot) return;
   
-    // Ignore messages not starting with the prefix (in config.json)
+    // return if message does not match prefix (in command)
     if (message.content.indexOf(client.config.prefix) !== 0) return;
   
-    // Our standard argument/command name definition.
+    // Defining what are arguments and commands
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
   
-    // Grab the command data from the client.commands Enmap
+    // Get the command data from the client.commands Enmap
     const cmd = client.commands.get(command);
   
-    // If that command doesn't exist, silently exit and do nothing
+    // If command does not exist return
     if (!cmd) return;
   
     // Run the command
